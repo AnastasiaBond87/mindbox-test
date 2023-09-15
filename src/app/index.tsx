fc87b4a0d95a917ccd580d7ee2d47a21b2be3f84
krevetka87 <bonanast4sia@yandex.ru>
2023-09-15 16:26:57 +0300

    build: install deps

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Grid, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="sm" sx={{ py: 3 }}>
+      <Typography>todos</Typography>
+    </Container>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    css: true,
+    coverage: {
+      enabled: true,
+      all: true,
+      provider: 'v8',
+      exclude: [],
+      include: [],
+    },
+  },
+});
