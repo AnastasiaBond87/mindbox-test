c2e7e8c1e479cb4d04baf036c5d619b2ce067fe0
krevetka87 <bonanast4sia@yandex.ru>
2023-09-15 18:17:16 +0300

    fix: add store provider

--- src/components/TodoInput/index.tsx
+++ src/components/TodoInput/index.tsx
@@ -1,4 +1,9 @@
-import { TextField, styled } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
+import { Box, TextField, styled } from '@mui/material';
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import type { ITodo } from '@/shared/types';
+import { useAppDispatch } from '@/app/store/hooks';
+import { addTodo } from '@/app/store/slices/todoSlice';
 
 const StyledTextField = styled(TextField)({
   '& ::placeholder': {
@@ -13,12 +18,38 @@ const StyledTextField = styled(TextField)({
 });
 
 export default function TodoInput() {
+  const dispatch = useAppDispatch();
+  const [value, setValue] = useState('');
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { value } = event.target;
+    setValue(value.trim());
+  };
+
+  const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    if (value) {
+      const newTodo: ITodo = {
+        id: uuidv4(),
+        body: value,
+        completed: false,
+      };
+
+      dispatch(addTodo(newTodo));
+    }
+  };
+
   return (
-    <StyledTextField
-      placeholder="What need to be done?"
-      fullWidth
-      autoComplete="off"
-      size="medium"
-    />
+    <Box component="form" onSubmit={handleSubmit}>
+      <StyledTextField
+        placeholder="What need to be done?"
+        fullWidth
+        autoComplete="off"
+        size="medium"
+        value={value}
+        onChange={handleChange}
+      />
+    </Box>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from '@/app';
 import './index.css';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '@/app/theme';
+import { Provider } from 'react-redux';
+import { store } from '@/app/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
